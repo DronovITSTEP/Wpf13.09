d1ac02f21eebfce2f3d4b53ca7b1c239e60f3f50
ApxNord <nord-life17@yandex.ru>
2023-09-18 18:11:38 +0300

    Добавьте файлы проекта.

---
+++ Wpf13.09/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using System;
+
+namespace Wpf13._09
+{
+    class Person {
+        public string Surname { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender {  get; set; }
+        public List<string> ListInteres { get; set; }
+        public string Country { get; set; }
+        public override string ToString()
+        {
+            return $"Фамилия: {Surname} Имя: {FirstName} Отчество: {LastName}\n" +
+                $"Дата Рождения: {BirthDate.ToShortDateString()}\n" +
+                $"Пол: {Gender}\nСписок интересов:{Inter()}\n";
+        }
+        private string Inter()
+        {
+            string str = "";
+            foreach (string i in ListInteres)
+                str += i + " ";
+
+            return str;
+        }
+
+    }
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person;
+        List<string> items;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            items = new List<string>
+           {
+               "Россия",
+               "Америка",
+               "Турция"
+           };
+            CountryBox.ItemsSource = items;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            person = new Person
+            {
+                FirstName = FirstName.Text, LastName = LastName.Text,
+                Surname = SurName.Text, BirthDate = BirthDate.DisplayDate,
+                Country = items[CountryBox.SelectedIndex],
+                ListInteres = new List<string>()
+                
+            };
+
+            if (Male.IsChecked == true) { person.Gender = Male.Content.ToString(); }
+            else { person.Gender = Female.Content.ToString(); }
+
+            foreach (CheckBox i in Interes.Children)
+            {
+                if (i.IsChecked == true) 
+                    person.ListInteres.Add(i.Content.ToString());
+            }
+            
+            MessageBox.Show(person.ToString());            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            FirstName.Text = string.Empty;
+            LastName.Text = string.Empty;
+            SurName.Text = string.Empty;
+            BirthDate.Text = string.Empty;
+
+            Male.IsChecked = false;
+            Female.IsChecked = false;
+
+            CountryBox.SelectedIndex = -1;
+
+            foreach (CheckBox i in Interes.Children)
+            {
+                i.IsChecked = false;
+            }
+
+        }
+    }
+}
---
+++ Wpf15.09/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf15._09
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random random = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Yes_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вот и хорошо. Продолжай работать");
+        }
+
+        private void No_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //Console.WriteLine("Мышка в кнопке");
+
+            /*double windowWidth = Canva.ActualWidth;
+            double windowHeight = Canva.ActualHeight;
+
+            double buttonWidth = NoButton.ActualWidth;
+            double buttonHeight = NoButton.ActualHeight;
+
+            double maxX = windowWidth - buttonWidth;
+            double maxY = windowHeight - buttonHeight;
+
+            double randomX = random.NextDouble() * maxX;
+            double randomY = random.NextDouble() * maxY;
+
+            Canvas.SetLeft(NoButton, randomX);
+            Canvas.SetTop(NoButton, randomY);*/
+            if (NoButton.Content.ToString() == "Нет")
+            {
+                NoButton.Content = "Да";
+                YesButton.Content = "Нет";
+            }
+            else
+            {
+                NoButton.Content = "Нет";
+                YesButton.Content = "Да";
+            }
+        }
+    }
+}
